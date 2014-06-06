4e823a6e378f0740f952060dd31fd3db132bb2e4
Wiktor Matuszewski <pywkm@wukaem.pl>
2014-06-06 21:33:49 +0200

    Dodany skrypt do szukania polskich alfametyków.

---
+++ examples/alphametrics_pl.py
@@ -0,0 +1,20 @@
+# !/usr/bin/python3
+
+from alphametics import solve
+
+
+def main():
+    # RAZ + DWA == TRZY
+    # 273 + 957 == 1230
+
+    # RAZ + DWA + TRZY == SZESC
+    # 360 + 756 + 9302 == 10418
+
+    # RAZ + DWA + DWA == PIEC
+    # 145 + 964 + 964 == 2073
+
+    print(solve("CZAS + NIE + LATA + WCALE + ALE + LECI == STALE"))
+
+
+
+if __name__ == "__main__": main()
